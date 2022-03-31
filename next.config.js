df6ff1d411267822b2a45d3f74e07c5e097754cf
Peter Berryman <peterb@deeson.co.uk>
2022-03-31 14:18:18 +0100

    Test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.twig$/,
+      use: {
+        loader: 'twigjs-loader',
+      }
+    });
+    config.resolve.alias['@components'] = path.join(__dirname, 'node_modules', 'fsa-pattern-library-assets', 'dist', 'components');
+
+    return config
+  },
 }
 
 module.exports = nextConfig
---
+++ pages/menu-write.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import parse, { domToReact } from 'html-react-parser';
+import button from '@components/search/SortBy/sortBy.html.twig';
+
+export async function getServerSideProps () {
+  const fs = require('fs');
+  const d = new Date();
+  const menu = { date: d.toLocaleString(), menu: { random: Math.random(), menu: 'things' } } 
+
+  fs.writeFileSync('data/menu.json', JSON.stringify(menu, null, 4));
+  return { props: {menu: menu}};
+}
+
+export default function Home(menu) {
+  console.log(menu);
+  return (
+    <div className="Foo">Something</div>
+  )
+}
