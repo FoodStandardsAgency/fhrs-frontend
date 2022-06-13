c4a25c8197034974d1dcfe75756aff4b7274eaa8
Emily <emilyh@deeson.co.uk>
2022-06-13 10:26:26 +0100

    FFHRS-40: Create react class for v2 api - Create class - Add test examples to index.js (TODO: remove once more examples added to the site).

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,8 @@ import Image from 'next/image'
 import sortBy from '@components/components/search/SortBy/sortBy.html.twig';
 import input from '@components/components/form/InputField/inputField.html.twig';
 import TwigTemplate from '../lib/parse.js';
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect, useMemo, componentDidMount } from 'react';
+import api from '../lib/api.js';
 
 export async function getStaticProps () {
   const fs = require('fs');
@@ -29,7 +30,7 @@ const sort = {
       "text": "Alphabetical",
       "value": "alpha"
     }
-  ], 
+  ],
   sort_by: 'Sort by'
 };
 const sortAttribs = [
@@ -82,7 +83,59 @@ export default function Home(menu) {
 
   useMemo(() => { console.log(`Business ${business}`);  }, [business]);
   useMemo(() => { console.log(`Address ${address}`);  }, [address]);
-  
+
+// Test data for the ratings api
+//   const data = async () => {
+//     // Regions
+//     console.log('Regions');
+//     console.log("pages", await api.setType('regions', {pageNumber: 2, pageSize: 5}).getResults());
+//     console.log("basicPages", await api.setType('regions', {pageNumber: 2, pageSize: 5, basic: true}).getResults());
+//     console.log("basic", await api.setType('regions', {basic: true}).getResults());
+//     console.log("all", await api.setType('regions').getResults());
+//     console.log("single", await api.setType('regions', {id: 5}).getResults());
+//     // Authorities
+//     console.log('Authorities');
+//     console.log("pages", await api.setType('authorities', {pageNumber: 2, pageSize: 5}).getResults());
+//     console.log("basicPages", await api.setType('authorities', {pageNumber: 2, pageSize: 5, basic: true}).getResults());
+//     console.log("basic", await api.setType('authorities', {basic: true}).getResults());
+//     console.log("all", await api.setType('authorities').getResults());
+//     console.log("single", await api.setType('authorities', {id: 5}).getResults());
+//     // Business Types
+//     console.log('Business Types');
+//     console.log("pages", await api.setType('businessTypes', {pageNumber: 2, pageSize: 5}).getResults());
+//     console.log("basicPages", await api.setType('businessTypes', {pageNumber: 2, pageSize: 5, basic: true}).getResults());
+//     console.log("basic", await api.setType('businessTypes', {basic: true}).getResults());
+//     console.log("all", await api.setType('businessTypes').getResults());
+//     console.log("single", await api.setType('businessTypes', {id: 5}).getResults());
+//     // Countries
+//     console.log('Countries');
+//     console.log("pages", await api.setType('countries', {pageNumber: 2, pageSize: 5}).getResults());
+//     console.log("basicPages", await api.setType('countries', {pageNumber: 2, pageSize: 5, basic: true}).getResults());
+//     console.log("basic", await api.setType('countries', {basic: true}).getResults());
+//     console.log("all", await api.setType('countries').getResults());
+//     console.log("single", await api.setType('countries', {id: 2}).getResults());
+//     // Establishments
+//     console.log('Establishments');
+//     console.log("basicPages", await api.setType('establishments', {pageNumber: 2, pageSize: 5, basic: true}).getResults());
+//     console.log("basic", await api.setType('establishments', {basic: true}).getResults());
+//     console.log("single", await api.setType('establishments', {id: 4}).getResults());
+//     console.log("withParams", await api.setType('establishments', {}, {name: 'mcdonalds', ratingKey: '1'}).getResults());
+//     // Scheme types
+//     console.log('Scheme types');
+//     console.log("all", await api.setType('schemetypes').getResults());
+//     // Sort Options
+//     console.log('Sort options');
+//     console.log("all", await api.setType('sortoptions').getResults());
+//     // Score descriptors
+//     console.log('Score descriptors');
+//     console.log("all", await api.setType('scoredescriptors', {establishmentId : '4'}).getResults());
+//     // Ratings
+//     console.log('Ratings');
+//     console.log("all", await api.setType('ratings').getResults());
+//     // Ratings operators
+//     console.log('Rating operators');
+//     console.log("all", await api.setType('ratingoperators').getResults());
+//   }
   return (
     <div>
       <TwigTemplate template={sortBy} values={sort} attribs={sortAttribs} />
