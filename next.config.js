ffbfc42fba425fcd86deb17c6117c3121a971c39
Peter Berryman <peterb@deeson.co.uk>
2022-09-21 16:07:20 +0100

    Incorrect domains value

--- next.config.js
+++ next.config.js
@@ -20,6 +20,7 @@ const nextConfig = {
         ],
       ),
     )
+    console.log(config);
     config.output.publicPath = '/';
     config.resolve.alias['@components'] = path.join(__dirname, 'node_modules', 'fsa-pattern-library-assets', 'dist');
 
@@ -28,7 +29,6 @@ const nextConfig = {
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    domains: ['www.rct.uk']
   },
   assetPrefix: undefined
 }
