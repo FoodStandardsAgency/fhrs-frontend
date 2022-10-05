cf88fbcbc5dadaa18aace5fdb93885c71be9541a
Peter Berryman <peterb@deeson.co.uk>
2022-10-05 11:20:17 +0100

    Make netlify happy

--- pages/embed.js
+++ pages/embed.js
@@ -1,8 +1,7 @@
 function EmbedTest() {
-
   return (
     <>
-      <script src="http://localhost:3000/embed/embed-badge.js" data-rating="5" data-rating-style="3" data-fhis="false" data-welsh="false" />
+      <script src="/embed/embed-badge.js" data-rating="5" data-rating-style="3" data-fhis="false" data-welsh="false" defer />
     </>
   )
 }
