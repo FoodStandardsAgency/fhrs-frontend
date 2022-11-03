da4bb168cc370a2d01270296927b1e54a5374b90
Peter Berryman <peterb@deeson.co.uk>
2022-11-03 11:09:36 +0000

    Still trying to fix language weirdness

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,34 @@ import {appWithTranslation} from 'next-i18next';
 import {HistoryProvider} from '../context/History'
 import Head from 'next/head'
 import "../components/business-table.css";
+import { useEffect } from 'react'
+import { useRouter } from 'next/router';
 
 
 function FhrsApp({Component, pageProps}) {
+  const router = useRouter()
+
+  useEffect(() => {
+    // Some kind of weirdness is preventing azure giving a full URL in the header.
+    // Force one in.
+    const handleStop = () => {
+      const lang = document.querySelectorAll('.header__language a');
+      lang.forEach((a) => {
+        let href = a.getAttribute('href');
+        if (!href.includes('?') && typeof window === 'object' && window.location?.search.length) {
+          href += window.location.search;
+        }
+        a.setAttribute('href', href);
+      });
+    }
+
+    router.events.on('routeChangeComplete', handleStop)
+
+    return () => {
+      router.events.off('routeChangeComplete', handleStop)
+    }
+  }, [router]);
+
   return (
     <>
       <Head>
